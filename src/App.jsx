609dae429718d86965552e8e0850da9a81097c2e
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-10-02 01:54:29 +0530

    Homepage done

--- src/App.jsx
+++ src/App.jsx
@@ -21,19 +21,26 @@ function App() {
   console.log(url);
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
+  //res stored in store --for img
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res));
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+
+      dispatch(getApiConfiguration(url));
     });
   };
 
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         {/* media type : means its is movie or tv show  */}
@@ -42,7 +49,7 @@ function App() {
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,9 +1,71 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Img from "../../../components/lazyLoadImgae/Img";
+import { useSelector } from "react-redux";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import "./style.scss";
 
+import useFetch from "../../../hooks/useFetch";
+
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  const { url } = useSelector((state) => state.home);
+
+  // state for to change the background of herobanner page everytime on refresh
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+
+  //api call
+
+  const { data, loading } = useFetch("/movie/upcoming");
+
+  // jb api call hogi toh data we got uske baad humein useefect cal krwana hai that why we put data in dependices
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
+
+  // ..
+  const navigate = useNavigate();
+
+  //check search input not be empty and when hit the enter than only hit the api
+  const seacrhQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      // query is parameter that you write in search
+      navigate(`/search/${query}`);
+    }
+  };
+
+  return (
+    <div className="heroBanner">
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      )}
+      <div className="opacity-layer"></div>
+
+      <ContentWrapper>
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to discover. Explore now.
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or tv show...."
+              // whatever you write on search it will automatically set on query
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={seacrhQueryHandler}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default HeroBanner;
