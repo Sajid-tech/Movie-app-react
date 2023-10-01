08d8b02d72d1fb4f9d61c27655bda7ed29483be2
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-10-02 00:27:26 +0530

    react-redux and api testing done

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+
+import { fetchDataFromApi } from "./utils/api";
+
+function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
+  };
+
+  return (
+    <>
+      <div className="App">
+        App
+        {url?.total_pages}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/404/pageNotfound.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./style.scss";
+
+export const pageNotfound = () => {
+  return <div>pageNotfound</div>;
+};
