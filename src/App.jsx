73df5df34ec02029d1d2070d5862b9f56885bad2
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-10-02 17:25:05 +0530

    footer done ..

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </BrowserRouter>
   );
 }
